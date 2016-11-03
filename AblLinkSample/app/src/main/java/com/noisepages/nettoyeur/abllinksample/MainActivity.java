de8e72aa10b021393605e467faad9102849a1409
Peter Brinkmann <peter.brinkmann@gmail.com>
2016-11-04 00:08:15 +0100

    Added Pd scaffolding and metronome patch.

--- AblLinkSample/app/src/main/java/com/noisepages/nettoyeur/abllinksample/MainActivity.java
+++ AblLinkSample/app/src/main/java/com/noisepages/nettoyeur/abllinksample/MainActivity.java
@@ -1,13 +1,215 @@
 package com.noisepages.nettoyeur.abllinksample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.puredata.android.io.AudioParameters;
+import org.puredata.android.service.PdPreferences;
+import org.puredata.android.service.PdService;
+import org.puredata.core.PdBase;
+import org.puredata.core.PdReceiver;
+import org.puredata.core.utils.IoUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AblLinkSample";
+
+    private PdService pdService = null;
+
+    private Toast toast = null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    private void toast(final String msg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (toast == null) {
+                    toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT);
+                }
+                toast.setText(TAG + ": " + msg);
+                toast.show();
+            }
+        });
+    }
+
+    private PdReceiver receiver = new PdReceiver() {
+
+        private void pdPost(String msg) {
+            toast("Pure Data says, \"" + msg + "\"");
+        }
+
+        @Override
+        public void print(String s) {
+            toast(s);
+        }
+
+        @Override
+        public void receiveBang(String source) {
+            pdPost("bang");
+        }
+
+        @Override
+        public void receiveFloat(String source, float x) {
+            pdPost("float: " + x);
+        }
+
+        @Override
+        public void receiveList(String source, Object... args) {
+            pdPost("list: " + Arrays.toString(args));
+        }
+
+        @Override
+        public void receiveMessage(String source, String symbol, Object... args) {
+            pdPost("message: " + Arrays.toString(args));
+        }
+
+        @Override
+        public void receiveSymbol(String source, String symbol) {
+            pdPost("symbol: " + symbol);
+        }
+    };
+
+    private final ServiceConnection pdConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            pdService = ((PdService.PdBinder) service).getService();
+            initPd();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            // this method will never be called
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AudioParameters.init(this);
+        PdPreferences.initPreferences(getApplicationContext());
+        initGui();
+        bindService(new Intent(this, PdService.class), pdConnection, BIND_AUTO_CREATE);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cleanup();
+    }
+
+    private void initGui() {
         setContentView(R.layout.activity_main);
+        final CheckBox connectBox = (CheckBox) findViewById(R.id.connectBox);
+        connectBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PdBase.sendFloat("connect", connectBox.isChecked() ? 1 : 0);
+            }
+        });
+    }
+
+    private void initPd() {
+        Resources res = getResources();
+        File patchFile = null;
+        try {
+            PdBase.setReceiver(receiver);
+            PdBase.subscribe("android");
+            InputStream in = res.openRawResource(R.raw.metronome);
+            patchFile = IoUtils.extractResource(in, "metronome.pd", getCacheDir());
+            PdBase.openPatch(patchFile);
+            startAudio();
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+            finish();
+        } finally {
+            if (patchFile != null) patchFile.delete();
+        }
+    }
+
+    private void startAudio() {
+        String name = getResources().getString(R.string.app_name);
+        try {
+            pdService.initAudio(-1, 0, 2, -1);   // negative values will be replaced with defaults/preferences
+            pdService.startAudio(new Intent(this, MainActivity.class), R.drawable.icon, name, "Return to " + name + ".");
+        } catch (IOException e) {
+            toast(e.toString());
+        }
+    }
+
+    private void stopAudio() {
+        pdService.stopAudio();
+    }
+
+    private void cleanup() {
+        try {
+            stopAudio();
+            unbindService(pdConnection);
+        } catch (IllegalArgumentException e) {
+            // already unbound
+            pdService = null;
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
