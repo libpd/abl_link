e4124254d78f9320853845d3fc1178a2f35d1c43
Peter Brinkmann <peter.brinkmann@gmail.com>
2016-11-03 18:54:45 +0100

    .

--- android/Android.mk
+++ android/Android.mk
@@ -1,7 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+#---------------------------------------------------------------
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := pd
-LOCAL_EXPORT_C_INCLUDES := ../../pd-for-android/PdCore/jni/libpd/pure-data/src
-LOCAL_SRC_FILES := ../../pd-for-android/PdCore/libs/$(TARGET_ARCH_ABI)/libpd.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../pd-for-android/PdCore/jni/libpd/pure-data/src
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../pd-for-android/PdCore/libs/$(TARGET_ARCH_ABI)/libpd.so
 ifneq ($(MAKECMDGOALS),clean)
     include $(PREBUILT_SHARED_LIBRARY)
 endif
@@ -10,7 +14,7 @@ endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := abl_link_tilde
-LOCAL_CFLAGS := -DPD -I../external/android-ifaddrs -I../external/link/include -I../external/link/modules/asio-standalone/asio/include -DLINK_PLATFORM_LINUX=1 -DABL_LINK_OFFSET_MS=15
+LOCAL_CFLAGS := -DPD -I../external/android-ifaddrs -I../external/link/include -I../external/link/modules/asio-standalone/asio/include -DLINK_PLATFORM_LINUX=1 -DABL_LINK_OFFSET_MS=23
 LOCAL_CPPFLAGS := -std=c++11 -Wno-multichar -fexceptions -Werror
 LOCAL_SRC_FILES := ../external/abl_link~.cpp ../external/abl_link_instance.cpp ../external/android-ifaddrs/ifaddrs.cpp
 LOCAL_SHARED_LIBRARIES = pd
